0ced608e733b800ebd9eb0e680125af144962b1c
cmr6689 <cmr6689@g.rit.edu>
2019-03-29 13:46:40 -0400

    finished in lab

--- src/connectfour/gui/ConnectFourGUI.java
+++ src/connectfour/gui/ConnectFourGUI.java
@@ -1,10 +1,23 @@
 package connectfour.gui;
 
+import connectfour.ConnectFourException;
 import connectfour.client.ConnectFourBoard;
+import connectfour.client.ConnectFourNetworkClient;
 import connectfour.client.Observer;
+import connectfour.server.ConnectFour;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.util.List;
 
 /**
@@ -16,8 +29,10 @@ import java.util.List;
  */
 public class ConnectFourGUI extends Application implements Observer<ConnectFourBoard> {
 
+    ConnectFourNetworkClient client;
+
     @Override
-    public void init() {
+    public void init() throws ConnectFourException {
         try {
             // get the command line args
             List<String> args = getParameters().getRaw();
@@ -25,8 +40,8 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
             // get host info and port from command line
             String host = args.get(0);
             int port = Integer.parseInt(args.get(1));
-
-            // TODO
+            ConnectFourBoard board = new ConnectFourBoard();
+            client = new ConnectFourNetworkClient(host, port, board);
         } catch(NumberFormatException e) {
             System.err.println(e);
             throw new RuntimeException(e);
@@ -40,11 +55,29 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
      * @throws Exception if there is a problem
      */
     public void start( Stage stage ) throws Exception {
-        // TODO
+        Image image = new Image(new FileInputStream("/Users/cameronriu/Documents/Spring 2019/Comp Sci 2/Labs/lab08-cmr6689/src/connectfour/gui/empty.png"));
+        ImageView imgView = new ImageView(image);
 
+        VBox vbox = new VBox(imgView);
+
+        Button button = new Button("", vbox);
+        button.setPrefSize(64, 64);
+        button.centerShapeProperty();
+
+        Label label = new Label("Welcome");
+
+        GridPane gridPane = new GridPane();
+        //TODO
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPrefSize(500, 500);
+        borderPane.setCenter(button);
+        borderPane.setBottom(label);
+
+        Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
         stage.show();
-
-        // TODO: call startListener() in ConnectFourNetworkClient here
+        client.startListener();
     }
 
     /**
